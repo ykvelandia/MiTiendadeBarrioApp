a858cc84546327ff042e5f5de45dc989fa97f29d
ykvelandia <velandia416@gmail.com>
2023-12-18 15:35:07 -0500

    My main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import model.*;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" ¡Bienvenid@ a Mi Tienda App!");
+        Main main = new Main();
+        main.runMenu();
+    }
+
+    //Crea una función o método que te permita mostrar las opciones del menu:
+    private void displayMenu() {
+
+        System.out.println(
+                """
+                        |o|                         /////////////\\\\\\
+                        |o|                        (((((((((((((   \\\\\\
+                        |o|                        ))) ~~      ~~   (((
+                        |o|                        ((( (*)     (*)  )))
+                        |o|                        )))     <        (((\s
+                        |o|                        ((( '\\______/`   )))\s
+                        |o|                        )))\\___________/(((\s
+                        |o|                        (((   _)  (_    )))\s\s
+                        |o|                              /\\__/\\""");
+        System.out.println("±----------------------------------------±");
+        System.out.println("|   Administrador Mi Tienda de Barrio    |");
+        System.out.println("±----------------------------------------±");
+        System.out.println("---Ingrese una opcion de las siguientes: ---");
+        System.out.println("±----------------------------------------±");
+        System.out.println("1. Agregar producto                      |");
+        System.out.println("2. Eliminar producto                     |");
+        System.out.println("3. Actualizar producto                   |");
+        System.out.println("4. Ver todos los productos               |");
+        System.out.println("5. Ver Inventario                        |");
+        System.out.println("6. Ver productos en orden alfabético     |");
+        System.out.println("7. Salir                                 |");
+        System.out.println("±----------------------------------------±");
+        System.out.print("   Ingresa tu opción:    (1 - 7)  ");
+    }
+
+    //Menu Opciones de App Mi Tienda de Barrio
+    private  void handleUserChoice(int choice) {
+
+        switch (choice) {
+            case 1 -> AddProduct.addProduct();
+            case 2 -> RemoveProduct.removeProduct();
+            case 3 -> UpdateProduct.updateProduct();
+            case 4 -> ProductsArray.showProducts();
+            case 5 -> Inventario.verInventario();
+            case 6 -> ProductsArray.alphabeticOrder();
+            case 7 -> System.out.println("Saliendo...");
+            default -> System.out.println("Opción invalida. Por favor intenta de nuevo.");
+        }
+
+    }
+
+    //Implementa una función que te permita mostrar el menu y capturar la opción seleccionada por el usuario:
+    public void runMenu() {
+        int choice;
+        Scanner scanner= new Scanner(System.in);
+        do {
+            displayMenu();
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+            handleUserChoice(choice);
+        } while (choice != 8);
+    }
+
+}
