479a20455bc922d8fe1aded8131818f24d5179c7
ykvelandia <velandia416@gmail.com>
2024-02-27 22:39:27 -0500

    security

---
+++ src/main/java/org/example/security/encrypt/PasswordEncryptionService.java
@@ -0,0 +1,8 @@
+package org.example.security.encrypt;
+
+public interface PasswordEncryptionService {
+
+    String encrypt(String password);
+    boolean isPasswordMatch(String password, String encryptedPassword);
+
+}
