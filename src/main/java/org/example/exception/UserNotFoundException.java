4167bab18aac92687e4bdb4efc8037ec318f17c2
ykvelandia <velandia416@gmail.com>
2024-02-27 22:38:54 -0500

    exceptions

---
+++ src/main/java/org/example/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package org.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class UserNotFoundException extends ResponseStatusException {
+    public UserNotFoundException(String id) {
+        super(HttpStatus.NOT_FOUND, "user with ID: " + id + " not found");
+    }
+}
