089ffd6b0166a9d3d6958e7a15ffb82de24f3f96
ykvelandia <velandia416@gmail.com>
2024-03-21 13:00:28 -0500

    utils

---
+++ src/main/java/org/example/utils/Constants.java
@@ -0,0 +1,11 @@
+package org.example.utils;
+
+public interface Constants {
+
+    String CLAIMS_ROLES_KEY = "ada_roles";
+    int TOKEN_DURATION_MINUTES = 1600;
+    String ADMIN_ROLE = "ADMIN";
+    String USER_ROLE = "USER";
+    String MISSING_TOKEN_ERROR_MESSAGE = "Missing or wrong token";
+    String TOKEN_EXPIRED_MALFORMED_ERROR_MESSAGE = "Token expired ";
+}
