87f7f9980321a399ce0ded475181380b33e69555
ykvelandia <velandia416@gmail.com>
2024-03-21 12:56:52 -0500

    exceptions

---
+++ src/main/java/org/example/exception/InvalidCredentialsException.java
@@ -0,0 +1,10 @@
+package org.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class InvalidCredentialsException  extends ResponseStatusException {
+    public InvalidCredentialsException(){
+        super(HttpStatus.UNAUTHORIZED, "Invalid username or password");
+    }
+}
